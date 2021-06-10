081d5aa1a70753be953404e411b0ea4a05cb91dd
Nicolas Ferreira <nicolas.ferreiraxn10@gmail.com>
2021-06-10 18:00:09 -0300

    Swagger aplicado

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerJsonFile from "./swagger.json";
 
 const app = express();
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJsonFile));
 
 app.use(express.json());
 
